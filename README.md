410566bc7320c8788542381747da7015188305dd
Neeraj20088 <77206848+Neeraj20088@users.noreply.github.com>
2022-05-08 15:44:53 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C30-Class
+# PiratesInvasionStage-2.5
+created multiple cannonballs.
---
+++ js/Boat.js
@@ -0,0 +1,22 @@
+class Boat {
+
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    this.image = loadImage("assets/boats.png")
+    World.add(world, this.body);
+  }
+  display() {
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    fill("brown");
+    imageMode(CENTER);
+    image(this.image,0, 0, this.width, this.height);
+    pop();
+  }
+}
